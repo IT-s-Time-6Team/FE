6271241629e681010e11d95e9e918c44c30a322f
OrangeKim04 <queemkimu@naver.com>
2025-03-31 14:21:15 +0900

    feat: 제목 추가

--- src/pages/chatRoomExitPage.tsx
+++ src/pages/chatRoomExitPage.tsx
@@ -1,8 +1,18 @@
+import styled from '@emotion/styled';
 const ChatRoomExitPage = () => {
   return (
     <>
-      <p>채팅룸 종료</p>
+      <Title>채팅룸이 종료되었습니다!</Title>
+      <SubTitle>대화는 즐거우셨나요? 요약 결과를 보여드릴게요.</SubTitle>
     </>
   );
 };
 export default ChatRoomExitPage;
+const Title = styled.p`
+  font-size: 20px;
+  font-weight: 600;
+`;
+const SubTitle = styled.p`
+  font-size: 14px;
+  font-weight: 500;
+`;
