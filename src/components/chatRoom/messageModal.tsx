190f9125aeb93f4275b14be920c30ab349639f5e
OrangeKim04 <queemkimu@naver.com>
2025-04-03 19:29:48 +0900

    refactor: 채팅룸 종료 메시지를 객체로 정리하여 재사용

--- src/components/chatRoom/messageModal.tsx
+++ src/components/chatRoom/messageModal.tsx
@@ -1,13 +1,28 @@
 import styled from '@emotion/styled';
 import { Title, SubTitle } from '@components/shared/TextStyles';
 import { Mask, ModalBody } from '@components/shared/ModalStyles';
+const messages = {
+  warning: {
+    title: '채팅룸 종료 5분 전!',
+    subTitle: '잠시 뒤에 채팅룸이 자동으로 종료돼요.',
+  },
+  closed: {
+    title: '방장이 채팅룸을 종료했어요.',
+    subTitle: '5초 후 요약 페이지로 이동할게요.',
+  },
+  ended: {
+    title: '채팅룸이 종료되었어요.',
+    subTitle: '5초 후 요약 페이지로 이동할게요.',
+  },
+};
+
 const MessageModal = () => {
   return (
     <>
       <Mask />
       <MessageModalBody>
-        <Title>채팅룸 종료 5분전</Title>
-        <SubTitle>잠시 뒤에 채팅룸이 자동으로 종료돼요.</SubTitle>
+        <Title>{messages.warning.title}</Title>
+        <SubTitle>{messages.warning.subTitle}</SubTitle>
       </MessageModalBody>
     </>
   );
