5cb1d893d3f4c8c93667c34b402c09eb3fa45a6e
OrangeKim04 <queemkimu@naver.com>
2025-04-03 10:24:34 +0900

    style: 오류 페이지에 아이콘 적용

--- src/pages/chatRoomExit/chatRoomExpiredPage.tsx
+++ src/pages/chatRoomExit/chatRoomExpiredPage.tsx
@@ -2,12 +2,12 @@ import styled from '@emotion/styled';
 import Button from '@components/chatRoomExit/Button';
 import { Container, Header } from '@components/shared/UIStyles';
 import { Title, SubTitle } from '@components/shared/TextStyles';
-
+import errorIcon from '../../assets/error_page_icon.svg';
 // 채팅룸 종료 오류 페이지
 const ChatRoomExpiredPage = () => {
   return (
     <Container>
-      <ProfileImage />
+      <ProfileImage src={errorIcon} />
       <Header>
         <Title>채팅룸이 종료되었어요.</Title>
         <SubTitle>이미 종료된 채팅룸이에요. 새 방을 만들어 볼까요?</SubTitle>
@@ -18,10 +18,7 @@ const ChatRoomExpiredPage = () => {
 };
 export default ChatRoomExpiredPage;
 
-const ProfileImage = styled.div`
-  width: 136px;
-  height: 136px;
-  background-color: #d9d9d9;
+const ProfileImage = styled.img`
   margin-top: 216px;
   margin-bottom: 23px;
   align-self: center;
