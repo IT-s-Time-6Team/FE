e2819146bd050e6fb5a93148a2cfdd41aa2916e1
OrangeKim04 <queemkimu@naver.com>
2025-04-05 19:42:25 +0900

    feat: 채팅룸 키워드 카드 컴포넌트 생성

---
+++ src/components/chatRoom/keywordCard.tsx
@@ -0,0 +1,26 @@
+import styled from '@emotion/styled';
+import { Mask, ModalBody } from '@components/shared/ModalStyles';
+import { Title, SubTitle } from '@components/shared/TextStyles';
+const KeywordCard = () => {
+  return (
+    <>
+      <Mask />
+      <KeywordModalBody>
+        <Title>#LOL</Title>
+        <KeywordSubTitle>Q. 롤에서 맞라인으로 나왔을 때 가장 싫은 챔피언은?</KeywordSubTitle>
+      </KeywordModalBody>
+    </>
+  );
+};
+export default KeywordCard;
+const KeywordModalBody = styled(ModalBody)`
+  width: 268px;
+  height: 282px;
+  align-items: center;
+  padding: 34px 29px;
+  padding-bottom: 90px;
+`;
+const KeywordSubTitle = styled(SubTitle)`
+  text-align: center;
+  line-height: 140%;
+`;
