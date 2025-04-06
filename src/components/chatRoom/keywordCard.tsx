f3764c2cb5b9d7173df44316144dfdd75eebc3e5
OrangeKim04 <queemkimu@naver.com>
2025-04-06 13:52:14 +0900

    chore: swiper 설치

--- src/components/chatRoom/keywordCard.tsx
+++ src/components/chatRoom/keywordCard.tsx
@@ -1,15 +1,12 @@
 import styled from '@emotion/styled';
-import { Mask, ModalBody } from '@components/shared/ModalStyles';
+import { ModalBody } from '@components/shared/ModalStyles';
 import { Title, SubTitle } from '@components/shared/TextStyles';
 const KeywordCard = () => {
   return (
-    <>
-      <Mask />
-      <KeywordModalBody>
-        <Title>#LOL</Title>
-        <KeywordSubTitle>Q. 롤에서 맞라인으로 나왔을 때 가장 싫은 챔피언은?</KeywordSubTitle>
-      </KeywordModalBody>
-    </>
+    <KeywordModalBody>
+      <Title>#LOL</Title>
+      <KeywordSubTitle>Q. 롤에서 맞라인으로 나왔을 때 가장 싫은 챔피언은?</KeywordSubTitle>
+    </KeywordModalBody>
   );
 };
 export default KeywordCard;
---
+++ src/components/chatRoom/keywordModal.tsx
@@ -0,0 +1,8 @@
+const keywordModal = () => {
+  return (
+    <>
+      <p></p>
+    </>
+  );
+};
+export default keywordModal;
