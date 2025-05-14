d4728f02b85f01af5a8528c1993c259bacddda4a
Lee hwan seuk <sioo1014@naver.com>
2025-05-14 14:00:44 +0900

    fix: zustand를 통한 유저 정보 전역 상태 관리

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig({
       '@assets': path.resolve(__dirname, 'src/assets'),
       '@hooks': path.resolve(__dirname, './src/hooks'),
       '@styles': path.resolve(__dirname, './src/styles'),
+      '@store': path.resolve(__dirname, './src/store'),
     },
   },
   server: {
