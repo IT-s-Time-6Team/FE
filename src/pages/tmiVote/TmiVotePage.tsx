b70b090c8ef58bf9549cb933c5ce464cee1a10f2
OrangeKim04 <queemkimu@naver.com>
2025-05-24 20:20:34 +0900

    feat: 닉네임 검색창 생성

--- src/pages/tmiVote/TmiVotePage.tsx
+++ src/pages/tmiVote/TmiVotePage.tsx
@@ -7,8 +7,26 @@ import { ModalPortal } from '@components/shared/ModalPortal';
 import InviteModal from '@components/chatRoom/InviteModal';
 import { Title, SubTitle } from '@components/shared/TextStyles';
 import styled from '@emotion/styled';
+import searchIcon from '@assets/v2/search.svg';
+import Button from '@components/chatRoomExit/Button';
+const nicknames = [
+  '하나',
+  '고구마',
+  '오이',
+  '감자',
+  '또리',
+  '바보',
+  '비비',
+  '루피',
+  '쵸파',
+  '고기',
+  '쿠키',
+];
+
 const TmiVotePage = () => {
   const [isInviteOpen, setIsInviteOpen] = useState<boolean>(false);
+  const [selectedIdx, setSelectedIdx] = useState(null);
+
   return (
     <ChatRoomContainer>
       <ChatRoomHeader>
@@ -20,8 +38,22 @@ const TmiVotePage = () => {
         <SubTitle>누가 TMI를 말했을지 맞춰보세요!</SubTitle>
       </Header>
       <img src={questionIcon} />
-
-      <TmiText>오늘 아침에 양치하다가 칫솔을 떨어뜨려서 새 칫솔로 교체했어요.</TmiText>
+      <GrayBox>
+        <TmiText>오늘 아침에 양치하다가 칫솔을 떨어뜨려서 새 칫솔로 교체했어요.</TmiText>
+      </GrayBox>
+      <TmiText>TMI의 주인은 누구일까요?</TmiText>
+      <SearchBox>
+        <Input placeholder='닉네임 검색하기' />
+        <img src={searchIcon} />
+      </SearchBox>
+      <NicknameBox>
+        {nicknames.map((item, idx) => (
+          <Nickname key={item} isSelected={selectedIdx === idx} onClick={() => setSelectedIdx(idx)}>
+            <TmiText>{item}</TmiText>
+          </Nickname>
+        ))}
+      </NicknameBox>
+      <Button text='투표하기' />
       {isInviteOpen && (
         <ModalPortal>
           <InviteModal onClose={() => setIsInviteOpen(false)} roomId='sdfs3' />
@@ -33,9 +65,51 @@ const TmiVotePage = () => {
 export default TmiVotePage;
 const InfoButton = styled.img`
   width: 24px;
-  height: 24px;
+  min-height: 24px;
   cursor: pointer;
 `;
 const TmiText = styled(SubTitle)`
   color: #3e3333;
+  text-align: center;
+  line-height: 150%;
+`;
+const GrayBox = styled.div`
+  width: 300px;
+  height: 80px;
+  background-color: #f9f9f9;
+  padding: 19px 24px;
+  borderradius: 12px;
+`;
+const SearchBox = styled.div`
+  width: 297px;
+  height: 41px;
+  padding: 10px 13px;
+  border-radius: 8px;
+  background: #f9f9f9;
+  box-sizing: border-box;
+  outline: none;
+  border: none;
+  display: flex;
+  justify-content: space-between;
+`;
+const Input = styled.input`
+  border: none;
+  outline: none;
+  background: #f9f9f9;
+  width: 90%;
+`;
+const NicknameBox = styled.div`
+  width: 300px;
+  height: 134px;
+  display: flex;
+  flex-wrap: wrap;
+  gap: 8px 9px;
+  overflow-y: auto;
+`;
+const Nickname = styled.div`
+  width: 93px;
+  padding: 10px 15px;
+  box-sizing: boreder-box;
+  border-radius: 8px;
+  background: #ebebeb;
 `;
