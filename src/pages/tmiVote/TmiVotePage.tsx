aec762794864262c5ab63cd5d1516d965656dacf
OrangeKim04 <queemkimu@naver.com>
2025-06-28 16:50:53 +0900

    feat: 닉네임 검색 기능 구현

--- src/pages/tmiVote/TmiVotePage.tsx
+++ src/pages/tmiVote/TmiVotePage.tsx
@@ -16,7 +16,7 @@ const nicknames = [
   '오이',
   '감자',
   '또리',
-  '바보',
+  '바보sldjfrn',
   '비비',
   '루피',
   '쵸파',
@@ -27,46 +27,71 @@ const nicknames = [
 const TmiVotePage = () => {
   const [isInviteOpen, setIsInviteOpen] = useState<boolean>(false);
   const [selectedIdx, setSelectedIdx] = useState<number | null>(null);
+  const [searchQuery, setSearchQuery] = useState<string>('');
   const navigate = useNavigate();
 
+  const filteredNicknames = nicknames.filter((nickname) =>
+    nickname.toLowerCase().includes(searchQuery.toLowerCase()),
+  );
+
   return (
-    <ChatRoomContainer>
+    <VoteRoomContainer>
       <ChatRoomHeader>
         <InfoButton onClick={() => setIsInviteOpen(true)} src={InfoIcon} alt='info' />
         <CloseButton>종료</CloseButton>
       </ChatRoomHeader>
-      <Header>
-        <Title>누구의 TMI일까?</Title>
-        <SubTitle>누가 TMI를 말했을지 맞춰보세요!</SubTitle>
-      </Header>
-      <img src={questionIcon} />
-      <GrayBox>
-        <TmiText>오늘 아침에 양치하다가 칫솔을 떨어뜨려서 새 칫솔로 교체했어요.</TmiText>
-      </GrayBox>
-      <TmiText>TMI의 주인은 누구일까요?</TmiText>
-      <SearchBox>
-        <Input placeholder='닉네임 검색하기' />
-        <img src={searchIcon} />
-      </SearchBox>
-      <NicknameBox>
-        {nicknames.map((item, idx) => (
-          <Nickname key={item} isSelected={selectedIdx === idx} onClick={() => setSelectedIdx(idx)}>
-            <PersonText isClicked={selectedIdx === idx} onClick={() => setSelectedIdx(idx)}>
-              {item}
-            </PersonText>
-          </Nickname>
-        ))}
-      </NicknameBox>
+      <VoteBox>
+        <Header>
+          <Title>누구의 TMI일까?</Title>
+          <SubTitle>누가 TMI를 말했을지 맞춰보세요!</SubTitle>
+        </Header>
+        <img src={questionIcon} />
+        <GrayBox>
+          <TmiText>오늘 아침에 양치하다가 칫솔을 떨어뜨려서 새 칫솔로 교체했어요.</TmiText>
+        </GrayBox>
+        <VoteBox gap='10px'>
+          <TmiText>TMI의 주인은 누구일까요?</TmiText>
+          <SearchBox>
+            <Input
+              placeholder='닉네임 검색하기'
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
+            />
+            <img src={searchIcon} />
+          </SearchBox>
+          <NicknameBox>
+            {filteredNicknames.map((item, idx) => (
+              <Nickname
+                key={item}
+                isSelected={selectedIdx === idx}
+                onClick={() => setSelectedIdx(idx)}
+              >
+                <PersonText isClicked={selectedIdx === idx}>{item}</PersonText>
+              </Nickname>
+            ))}
+          </NicknameBox>
+        </VoteBox>
+      </VoteBox>
       <Button text='투표하기' onClick={() => navigate('/rooms')} />
       {isInviteOpen && (
         <ModalPortal>
           <InviteModal onClose={() => setIsInviteOpen(false)} roomId='sdfs3' />
         </ModalPortal>
       )}
-    </ChatRoomContainer>
+    </VoteRoomContainer>
   );
 };
 export default TmiVotePage;
+const VoteRoomContainer = styled(ChatRoomContainer)`
+  justify-content: space-between;
+`;
+const VoteBox = styled.div<{ gap?: string }>`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: ${({ gap }) => gap || '20px'};
+`;
 
 const InfoButton = styled.img`
   width: 24px;
@@ -114,9 +139,11 @@ const NicknameBox = styled.div`
   flex-wrap: wrap;
   gap: 8px 9px;
   overflow-y: auto;
+  align-items: flex-start;
 `;
 const Nickname = styled.div<{ isSelected: boolean }>`
   width: 93px;
+  min-height: 41px;
   padding: 10px 15px;
   box-sizing: boreder-box;
   border-radius: 8px;
