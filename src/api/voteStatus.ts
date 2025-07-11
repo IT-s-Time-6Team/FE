c281f43debea7f91d8ac84b2f4c0048a4825bfb3
Lee hwan seuk <sioo1014@naver.com>
2025-07-11 15:36:13 +0900

    fix: 투표 상태 조회 api 수정

--- src/api/voteStatus.ts
+++ src/api/voteStatus.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const getVoteStatus = async (roomKey: string) => {
   try {
     const res = await axios.get(`/api/tmi/rooms/${roomKey}/status`, { withCredentials: true });
-    console.log('투표 정보 조회:', res);
+    console.log('투표 상태 조회:', res);
     return res.data;
   } catch (error: unknown) {
     console.error('error:', error);
